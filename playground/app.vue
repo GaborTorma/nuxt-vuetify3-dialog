97b3a3e7b8d488d29c8ff409d449c7382705866c
Gábor Torma <gabor@torma.dev>
2023-10-01 13:12:01 +0200

    feat: vuetify 3 added

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,7 @@
-<template>
-  <div>
-    Nuxt module playground!
-  </div>
-</template>
+<script setup lang="ts"></script>
 
-<script setup>
-</script>
+<template>
+	<v-app>
+		<v-app-bar color="primary" density="compact" title="Nuxt module playground!" />
+	</v-app>
+</template>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-	modules: ['../src/module'],
+	modules: ['vuetify-nuxt-module', '../src/module'],
 
 	vuetify3Dialog: {},
 
